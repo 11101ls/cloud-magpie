be50921238e63edcce5299c8dc0b5b51474a7297
liangshuang <3238832446@qq.com>
2022-11-24 17:30:46 +0800

    登录页面

--- src/views/login/Index.vue
+++ src/views/login/Index.vue
@@ -1,33 +1,36 @@
 <template>
-<!-- <el-form :label-position="labelPosition" label-width="100px" :model="formLabelAlign" style="max-width: 460px"
-  ref="ruleFormRef">
-  <el-form-item label="Name" prop="username">
-    <el-input v-model="formLabelAlign.username" />
-  </el-form-item>
-  <el-form-item label="Activity zone" prop="password">
-    <el-input v-model="formLabelAlign.password" />
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click="submitForm(ruleFormRef)">Create</el-button>
-    <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
-  </el-form-item>
-</el-form>
+<!--
 <el-button @click="handleToken">测试token</el-button> -->
 <div class="login">
-  <div class="bg-container">
-    <div class="bg-container-group">
-      <div class="item item-right"></div>
-      <div class="item item-left"></div>
-      <div class="item item-top"></div>
-      <div class="item item-bottom"></div>
-      <div class="item item-middle"></div>
-    </div>
-    <div class="bg-container-group">
-      <div class="item item-right"></div>
-      <div class="item item-left"></div>
-      <div class="item item-top"></div>
-      <div class="item item-bottom"></div>
-      <div class="item item-middle"></div>
+  <div class="container-box">
+    <div class="pic"></div>
+    <div class="content">
+      <h1>Hey,hello</h1>
+      <span>Enter the information you entered while registering</span>
+
+      <el-form :label-position="labelPosition" label-width="100px" :model="formLabelAlign" ref="ruleFormRef"
+        size="large">
+        <el-form-item label="username" prop="username">
+          <el-input v-model="formLabelAlign.username" />
+        </el-form-item>
+        <el-form-item label="Password" prop="password">
+          <el-input v-model="formLabelAlign.password" />
+        </el-form-item>
+        <el-form-item>
+          <div class="form-text-button">
+            <el-checkbox-group v-model="formLabelAlign.type">
+              <el-checkbox label="Remember me" name="type" />
+            </el-checkbox-group>
+            <el-button type="text"><span class="forgot-password">Forgot password?</span></el-button>
+          </div>
+          </el-form-item>
+          <el-form-item>
+            <!-- <el-button type="primary" @click="submitForm(ruleFormRef)">Create</el-button>
+            <el-button @click="resetForm(ruleFormRef)">Reset</el-button> -->
+            <div class="login-button">Login</div>
+          </el-form-item>
+          </el-form>
+
     </div>
   </div>
 </div>
@@ -36,12 +39,13 @@
 <script lang="ts" setup>
 import type { FormInstance, FormRules } from 'element-plus';
 import { reactive, ref } from 'vue';
-import { login, login2, token } from '@/api/modules/user'
-const labelPosition = ref('right');
+import { login, login2, token } from '@/api/modules/user';
+const labelPosition = ref('top');
 
 const formLabelAlign = reactive({
-  username: '',
-  password: '',
+  username: "",
+  password: "",
+  type: []
 });
 const ruleFormRef = ref<FormInstance>();
 const rules = reactive<FormRules>({
@@ -87,5 +91,66 @@ const handleToken = () =>
 <!-- <style src="./bg.scss" /> -->
 
 <style lang="scss">
+.login {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
 
+  .container-box {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 1000px;
+    height: 620px;
+    // background: #bf3;
+    display: flex;
+    padding: 15px 0;
+
+    .pic {
+      width: 49%;
+      height: 100%;
+      background: url(@/assets/imgs/login-bg.jpg) no-repeat;
+      background-size: contain;
+    }
+
+    .content {
+      // background: gray;
+      width: 52%;
+      height: 100%;
+      padding: 80px 70px 0 80px;
+
+      :v-deep(el-form-item__content) {
+        width: 100%;
+      }
+            }
+            }
+            }
+ .el-form .form-text-button {
+  width: 100%;
+   display: flex;
+   justify-content: space-between;
+ }
+    // ::v-deep(.form-text-button),::v-deep(.el-form-item--large) {
+    //   margin-bottom: 15px;
+    // }
+   .el-form-item{
+    height: 60px;
+      margin-bottom: 12px;
+    }
+        .forgot-password{
+          color: transparent;
+            font-weight: bold;
+          background: linear-gradient(45deg, rgba(#5ea1fa,1) 0%, rgba($nadeshiko-pink, 1) 100%);
+        background-clip: text;
+          -webkit-background-clip: text;
+        }
+  .login-button{
+    cursor: pointer;
+    background: $baby-blue-eyes;
+    text-align: center;
+    width: 100%;
+    border-radius: var(--el-input-border-radius, var(--el-border-radius-base));
+    background: linear-gradient(45deg, rgba(#5ea1fa, 1) 0%, rgba($nadeshiko-pink, 1) 100%);
+  }
 </style>
